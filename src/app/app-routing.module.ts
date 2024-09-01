664d71f15b0fe60e0b5721b332f29b9ab2ed2c6b
Oladele Oluwanishola <149659874+Oluwanishola12@users.noreply.github.com>
2024-09-01 19:17:42 +0100

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ServicesComponent } from './services/services.component';
+import { PropertiesComponent } from './properties/properties.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component: HomeComponent},
+  {path:'services', component: ServicesComponent},
+  {path:'properties', component: PropertiesComponent},
+  {path:'about', component: AboutComponent},
+  {path:'contact', component: ContactComponent},
+  {path:'home', component: HomeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ServicesComponent } from './services/services.component';
+import { PropertiesComponent } from './properties/properties.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ServicesComponent,
+    PropertiesComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
